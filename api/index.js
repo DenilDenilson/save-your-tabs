11f073219fe128cdf70e46bc000e7e6c149b8fd8
Denilson <jordanpreseleccion@gmail.com>
2026-02-20 21:34:33 -0500

    add test route and remove created at in backend

--- api/index.js
+++ api/index.js
@@ -1,48 +1,78 @@
 import express from 'express';
 import crypto from 'crypto';
 import { createClient } from '@libsql/client';
+import { error, timeStamp } from 'console';
 
 const app = express();
 app.use(express.json());
 
+// -------- TURSO --------------
 const db = createClient({
 	url: process.env.TURSO_DATABASE_URL,
 	authToken: process.env.TURSO_AUTH_TOKEN,
 });
+// -----------------------------
+
+// T E S T
+app.get('/test', async (req, res) => {
+	res.status(200).json({
+		status: 'ok',
+		message: 'Server is running',
+		timeStamp: Date.now(),
+	});
+});
 
 // S A V E
 app.post('/api/save', async (req, res) => {
-	const { urls } = req.body;
+	try {
+		const { urls } = req.body;
+		if (!Array.isArray(urls) || urls.length === 0) {
+			return res.status(400).json({ error: 'Invalid URLs' });
+		}
 
-	if (!Array.isArray(urls) || urls.length === 0) {
-		return res.status(400).json({ error: 'Invalid URLs' });
+		const id = crypto.randomBytes(6).toString('base64url');
+
+		await db.execute({
+			sql: 'INSERT INTO sessions (id, urls) VALUES (?, ?)',
+			args: [id, JSON.stringify(urls)],
+		});
+
+		res.json({ id });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: err });
 	}
-
-	const id = crypto.randomBytes(6).toString('base64url');
-
-	await db.execute({
-		sql: 'INSERT INTO sessions (id, urls, created_at) VALUES (?, ?, ?)',
-		args: [id, JSON.stringify(urls), Date.now()],
-	});
-
-	res.json({ id });
 });
 
 // R E S O L V E
 app.get('/api/resolve/:id', async (req, res) => {
-	const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-	const result = await db.execute({
-		sql: 'SELECT urls FROM sessions WHERE id = ?',
-		args: [id],
-	});
+		const result = await db.execute({
+			sql: 'SELECT urls FROM sessions WHERE id = ?',
+			args: [id],
+		});
 
-	if (result.rows.length === 0) {
-		return res.status(404).json({ error: 'Not found url' });
+		if (result.rows.length === 0) {
+			return res.status(404).json({ error: 'Not found url' });
+		}
+
+		const urls = JSON.parse(result.rows[0].urls);
+		res.json({ urls });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: err });
 	}
-
-	const urls = JSON.parse(result.rows[0].urls);
-	res.json({ urls });
 });
+
+// ------- L O C A L   S E R V E R --------
+if (process.env.NODE_ENV !== 'production') {
+	const PORT = 3000;
+
+	app.listen(PORT, () => {
+		console.log(`🚀 Local server running on http://localhost:${PORT}`);
+	});
+}
 
 export default app;
