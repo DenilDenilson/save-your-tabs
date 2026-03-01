5c2bc744eef7f7d5cc3c02e8c53cf0fa1a2dcf55
Denilson <jordanpreseleccion@gmail.com>
2026-02-28 19:59:08 -0500

    prepare extension for deploy

--- extension/content.js
+++ extension/content.js
@@ -1,10 +1,10 @@
-console.log('🔥 Content script cargado');
+// console.log('🔥 Content script cargado');
 
 window.addEventListener('message', (e) => {
 	if (e.source !== window) return;
 
 	if (e.data.type === 'OPEN_SESSION') {
-		console.log('OPEN_SESSION launched');
+		// console.log('OPEN_SESSION launched');
 		chrome.runtime.sendMessage({
 			type: 'OPEN_SESSION',
 			id: e.data.id,
